71a3503f74e6b26d214f42cd927b53277ae85e7a
Sameerkc3579 <sameer24102@iiitnr.edu.in>
2026-01-30 09:21:53 +0530

    updating links

--- src/app/constants/projects.js
+++ src/app/constants/projects.js
@@ -1,5 +1,5 @@
 export const projects = [
     { name: 'HimachaliVerse', isOnline: false, link: '#', img: '/himachali.png', gif: '/himachali.png', descriptionKey: 'kangri', skills: ['python', 'pytorch', 'transformers'] },
-    { name: 'Accident Detection', isOnline: true, img: '/accident.jpeg', gif: '/accident.jpeg', link: 'https://github.com/Sameerkc3579', descriptionKey: 'accident', skills: ['python', 'opencv', 'yolo'] },
-    { name: 'Somato', subtitle: 'Food Delivery Clone', img: '/somato.png', link: 'https://github.com/Sameerkc3579', descriptionKey: 'somato', skills: ['react', 'js', 'css'], fit: 'bg-contain bg-center' }
+    { name: 'Accident Detection', isOnline: true, img: '/accident.jpeg', gif: '/accident.jpeg', link: 'https://frontend-git-main-sameer-choudharys-projects-b87966dc.vercel.app/', descriptionKey: 'accident', skills: ['python', 'opencv', 'yolo'] },
+    { name: 'Somato', subtitle: 'Food Delivery Clone', img: '/somato.png', link: 'https://somato-new.vercel.app/', descriptionKey: 'somato', skills: ['react', 'js', 'css'], fit: 'bg-contain bg-center' }
 ];
